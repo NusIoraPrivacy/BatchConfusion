6d213fd9b5507e0089c8699d78ae60bf912db890
NusIoraPrivacy <llm_privacy@163.com>
2025-03-15 23:14:43 +0800

    privacy_extraction_with_fine-tunin_gpt_4o_mini

--- src/configs/templates.py
+++ src/configs/templates.py
@@ -1,4 +1,4 @@
-old_attr_extract_template = (
+old_attr_extract_template_1 = (
     "Please extract the sensitive attributes from a question. The sensitive attributes include: \n"
     "Personal identifiable information, "
     "race, religion, ethnicity, religion, beliefs, age, "
@@ -16,7 +16,7 @@ old_attr_extract_template = (
     "Sensitive attributes: "
 )
 
-attr_extract_template = (
+old_attr_extract_template_2 = (
     "Please extract all words or phrases in the question that indicate sensitive attributes.\n"
     "The sensitive attributes include: Personal identifiable information, "
     "job, race, religion, ethnicity, religion, beliefs, age, contact information, geolocation data, residency and citizenship status, date, festival, URL, "
@@ -39,6 +39,24 @@ attr_extract_template = (
     "A 19-year-old girl with a history of repeated pain over the medial canthus and chronic use of nasal decongestants presents with an abrupt onset of fever with chills and rigor, diplopia on lateral gaze, moderate proptosis, chemosis, and a congested optic disc on examination. Based on these symptoms, what is the most likely diagnosis?\n"
     "Sensitive attributes: "
     "[\"19-year-old\", \"girl\", \"repeated pain\", \"medial canthus\", \"chronic use\", \"nasal decongestants\", \"fever\", \"chills\", \"rigor\", \"diplopia\", \"moderate proptosis\", \"chemosis\", \"congested optic disc\"]"
+    "Question: {question}\n"
+    "Sensitive attributes: "
+)
+
+attr_extract_template = (
+    "Please extract all words or phrases in the question that indicate sensitive attributes.\n"
+    "The sensitive attributes include: Personal identifiable information, "
+    "job, race, religion, ethnicity, religion, beliefs, age, contact information, geolocation data, residency and citizenship status, date, festival, URL, "
+    "sexual orientation or practices, gender identity, third-person pronoun, disability, employment and income details, company, "
+    "physical health and mental health issues, clinical signs including medical conditions and physical examination data, behavioral data, "
+    "name of medicine, including specific drugs, drug classes, and general medication types (e.g., nasal decongestants, antihistamines, beta-blockers, etc), "
+    "financial data, vulnerable financial status, account details, loan information, tax records, contract, trading algorithm, confidential business information, "
+    "educational records, voting status, membership in a trade union, social media and digital footprint, "
+    "legal consultation and case status, Law enforcement jurisdiction and procedures, consumer rights, ownership details, criminal record or name, "
+    "biometric data, genetic information, legal proceedings and disputes, surveillance and monitoring data, military and security clearance.\n"
+    "Your task is to strictly extract phrases that refer to these sensitive attributes. Even if a term indirectly refers to a sensitive attribute (e.g., a drug category instead of a specific medicine name), it should be included in the list. The list of sensitive attributes provided is not exhaustive — if you encounter any word or phrase that could reasonably be considered sensitive information under privacy laws or common data protection standards (like GDPR, HIPAA, or similar frameworks), include it in the list. Prioritize any data that could identify a person, describe their personal circumstances, or reveal confidential, medical, financial, or legal information.\n"
+    "Please preserve the original words from the user's question to form the attribute list and do not convert full names to abbreviations or abbreviations to full names, and do not add any extra words. Retain all duplicates, regardless of form variations (e.g., burglarize, burglarized, burglarizing).\n"
+    "Please strictly return a list of phrases in the format of [\"attribute 1\", \"attribute 2\", ..., \"attribute n\"]. If there is no sensitive attribute, return an empty list []. Try to identify as much as phrases as possible.\n"
     "Question: {question}\n"
     "Sensitive attributes: "
 )
---
+++ src/data_util/gen_attrs_fine_tuning_gpt.py
@@ -0,0 +1,72 @@
+from data_util.gpt_utils import get_response
+from configs.data_configs import *
+from configs.templates import attr_extract_template # test new prompt
+from configs.key import _API_KEY
+
+from openai import OpenAI
+import json
+import copy
+import argparse
+import os
+from tqdm import tqdm
+current = os.path.dirname(os.path.realpath(__file__))
+root_path = os.path.dirname(os.path.dirname(current))
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--root_path", type=str, default=root_path)
+    parser.add_argument("--temperature", type=float, default=1,
+        help = "temperature for text generation")
+    parser.add_argument("--max_tokens", type=int, default=1000,
+        help = "max new token for text generation")
+    parser.add_argument("--gpt_model", type=str, default="ft:gpt-4o-mini-2024-07-18:personal::BBH2yexM") # "gpt-4o-mini-2024-07-18"
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == "__main__":
+    args = parse_args()
+    
+    num = 2000 # set number of samples to generate attributes
+    data_paths = ['financial_datasets/financial_datasets_combined.json',
+                 'legal-qa-v1/legal_qa_v1.json',
+                 'medical_o1_reasoning_SFT/medical_o1_sft.json']
+    output_paths = [f'financial_datasets/financial_datasets_combined_attr_{num}.json',
+                    f'legal-qa-v1/legal_qa_v1_attr_{num}.json',
+                    f'medical_o1_reasoning_SFT/medical_o1_sft_attr_{num}.json']
+    # data_path = 'medical_o1_reasoning_SFT/medical_o1_sft.json'
+    # output_path = 'medical_o1_reasoning_SFT/medical_o1_sft_attr.json'
+    for data_path, output_path in zip(data_paths, output_paths):
+        with open(f'{root_path}/data/{data_path}') as fin: 
+            # loading new data
+            data = json.load(fin)
+            data = data[:num] 
+        client = OpenAI(api_key=_API_KEY)
+        output_data = []
+        with tqdm(total=len(data), unit='batch') as pbar:
+            for sample in data:
+                question = sample["Question"]
+                prompt = attr_extract_template.format(question=question)
+                # print(prompt)
+                success = False
+                n_try = 0
+                while not success:
+                    result = get_response(client, prompt, args)
+                    try:
+                        prefix = "Sensitive attributes:"
+                        if result.startswith(prefix):
+                            result = result.replace(prefix, '')
+                        result = eval(result)
+                        success = True
+                    except Exception as e:
+                        n_try += 1
+                    if n_try >= 5:
+                        break
+                # print(result)
+                if success:
+                    output = copy.deepcopy(sample)
+                    output["private attributes"] = result
+                    output_data.append(output)
+                    with open(f'{args.root_path}/data/{output_path}', 'w', encoding="utf-8") as fout: # output new data
+                        json.dump(output_data, fout, indent=4, ensure_ascii=False)
+                pbar.update(1)
